4d5fbd7ae2c9f66ebc215651125e3cefcc8a490f
Nikita Sabfo <alpha.sabfo@gmail.com>
2021-09-17 23:27:17 +0200

    init commit

---
+++ converter.py
@@ -0,0 +1,18 @@
+import json
+
+
+with open('anime-list.json', encoding="utf-8") as json_file:
+    data = json.load(json_file)
+    for el in data:
+        el['target_type'] = 'Anime'
+        el['status'] = 'completed'
+        el['rewatches'] = 0
+        el['score'] = 0
+        el['text'] = None
+        del el['ya_id']
+        del el['list_id']
+        del el['updated_at']
+
+
+with open('result_list.json', "w", encoding="utf-8") as file:
+    json.dump(data, file, ensure_ascii=False)
