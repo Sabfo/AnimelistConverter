5384b49933f4841c4997d24364084566d94ddacd
fakelog <solodyankin.d@outlook.com>
2022-01-13 23:17:47 +0500

    добавил конвертацию списков

--- converter.py
+++ converter.py
@@ -1,11 +1,22 @@
 import json
 
+def status(list_id):
+    if list_id == 1:
+        return 'watching'
+    elif list_id == 2:
+        return 'planned'
+    elif list_id == 3:
+        return 'completed'
+    elif list_id == 4:
+        return 'dropped'
+    elif list_id == 5:
+        return 'completed'
 
 with open('anime-list.json', encoding="utf-8") as json_file:
     data = json.load(json_file)
     for el in data:
         el['target_type'] = 'Anime'
-        el['status'] = 'completed'
+        el['status'] = status(el['list_id'])
         el['rewatches'] = 0
         el['text'] = None
         del el['ya_id']
